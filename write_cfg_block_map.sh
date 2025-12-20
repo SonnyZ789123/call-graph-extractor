46f5ea713ee49f4954e0d58ba1764a7d68f2309f
SonnyZ789123 <yoran.mertens@telenet.be>
2025-12-20 19:42:05 +0100

    Point script to independent jdart-examples foo

--- write_cfg_block_map.sh
+++ write_cfg_block_map.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Hard-coded script to test the control flow graph generation for the library-application project
-
 set -e
 
 mvn -q -DskipTests=true package
-mvn exec:java -Dexec.mainClass="com.kuleuven.coverage.ControlFlowGraphCoverage" -Dexec.args="./target/classes/ \"<com.kuleuven._examples.Foo: int foo(int)>\""
+mvn exec:java -Dexec.mainClass="com.kuleuven.coverage.ControlFlowGraphCoverage" -Dexec.args="../../jdart-examples/out/production/jdart-examples \"<test.ControlFlowGraph.Test: int foo(int)>\""
 
 echo "✅ Done!"
